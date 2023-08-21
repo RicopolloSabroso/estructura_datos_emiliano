613d60e516e04c019f1121f01a9cafb81d238bc6
DESKTOP-SV53RGG\HP <emiseb123@outlook.com>
2023-08-21 09:02:43 -0600

    pila

---
+++ src/estructura/pila/Pila.java
@@ -0,0 +1,26 @@
+package estructura.pila;
+
+public class Pila {
+	private Object[] stack;
+	public Pila(int size) {
+		this.stack= new Object[size];
+		
+	}
+	public void push(Object pila) {
+		
+	}
+	public Object pop() {
+		
+	}
+	public int size() {
+		
+	}
+	public boolean isEmpty() {
+	
+	}
+	public Object peek() {
+		
+	}
+	
+	
+}
