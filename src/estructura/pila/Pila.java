5379da1cc6bb81b3456d5878527dd0d46c6c7c0d
DESKTOP-SV53RGG\HP <emiseb123@outlook.com>
2023-08-23 08:16:27 -0600

    Pila terminado

--- src/estructura/pila/Pila.java
+++ src/estructura/pila/Pila.java
@@ -2,25 +2,47 @@ package estructura.pila;
 
 public class Pila {
 	private Object[] stack;
+	private int size;
 	public Pila(int size) {
 		this.stack= new Object[size];
+		this.size=0;
 		
 	}
-	public void push(Object pila) {
-		
+	public void push(Object value) {
+		if(stack.length<=size) {
+			stack[size]=value;
+			size++;
+		}
 	}
 	public Object pop() {
-		
+		Object value=null;
+		if(!isEmpty()) {
+			size--;
+			value=stack[size];
+			stack[size]=null;
+		}
+		return value;
 	}
 	public int size() {
-		
+		return size;
 	}
 	public boolean isEmpty() {
-	
+		return size==0;
 	}
-	public Object peek() {
+	public Object peak() {
+		Object value=null;
+		if(size>0) {
+			value=stack[size-1];
+		}
+		return value;
 		
 	}
-	
+	public String toString() {
+		StringBuilder s=new StringBuilder();
+		for (int i=0; i<size; i++) {
+			s.append(stack[i]).append("->");
+		}
+		return s.toString();
+	}
 	
 }
