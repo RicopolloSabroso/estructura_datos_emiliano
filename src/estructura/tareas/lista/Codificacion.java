c469e63ad8860cd859c2bd564bd8452c49bba8ad
Emiliano <emiseb123@outlook.com>
2023-10-28 10:34:57 -0700

    polibio

--- src/estructura/tareas/lista/Codificacion.java
+++ src/estructura/tareas/lista/Codificacion.java
@@ -10,13 +10,23 @@ public class Codificacion {
 		Scanner leer = new Scanner(System.in);
 		LinkedList lista= new LinkedList();
 		String palabra=leer.nextLine();
-        System.out.println(xor(lista, palabra));
+        //System.out.println(xor(lista, palabra));
+		LinkedList separacion= pasarALista(palabra);
+		System.out.println(separacion);
+		System.out.println(polibio(lista,palabra));
 	}
 	public static void convertirBinario(LinkedList arreglo,String palabra) {
 		for (int i = 0; i < palabra.length(); i++) {
 			 int letra = (int) palabra.charAt(i);
 			arreglo.add(String.format("%8s", Integer.toBinaryString(letra)).replace(' ', '0'));
 		}    
+    }
+	public static LinkedList pasarALista(String palabra) {
+		LinkedList lista= new LinkedList();
+		for (int i = 0; i < palabra.length(); i++) {
+			lista.add(palabra.charAt(i));
+		}   
+		return lista;
     }
 	public static LinkedList xor(LinkedList arreglo,String palabra) throws Exception{
 		convertirBinario(arreglo,palabra);
@@ -35,6 +45,30 @@ public class Codificacion {
 			 resultado.add(codif);
 	      }
         return resultado;
+    }
+	public static LinkedList polibio(LinkedList arreglo,String palabra) throws Exception{
+		 String matrizPolibio [][] = {
+				{"A", "B", "C", "D", "E"},
+			    {"F", "G", "H", "I,J", "K"},
+			    {"L", "M", "N", "O", "P"},
+			    {"Q", "R", "S", "T", "U"},
+			    {"V", "W", "X", "Y", "Z"}};
+		for (int k = 0; k < palabra.length(); k++) {
+			for (int i = 0; i < matrizPolibio.length; i++) {
+				 for (int j = 0; j < matrizPolibio[0].length; j++) {
+					 if((String.valueOf(palabra.charAt(k))).equalsIgnoreCase("I")||(String.valueOf(palabra.charAt(k))).equalsIgnoreCase("J")) {
+						 arreglo.add(24);
+						 i=6;
+						 break;
+					 }else if(matrizPolibio [i][j].equalsIgnoreCase(String.valueOf(palabra.charAt(k)))){
+						 arreglo.add(String.valueOf(i+1)+String.valueOf(j+1));
+						 i=6;
+						 break;
+					 }
+			      }
+		      }
+		}
+        return arreglo;
     }
 
 }
