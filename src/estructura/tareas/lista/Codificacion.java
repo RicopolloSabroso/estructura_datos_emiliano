6e7de98cd5c6b64a562bd91c519f57cdd91655ce
DESKTOP-PI1VMT9\EmiSe <emiseb123@outlook.com>
2023-10-29 14:03:33 -0600

    ADFGVX

--- src/estructura/tareas/lista/Codificacion.java
+++ src/estructura/tareas/lista/Codificacion.java
@@ -1,8 +1,13 @@
 package estructura.tareas.lista;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
-
+//import estructura.lista.ArrayList;
 import estructura.lista.LinkedList;
+import estructura.pila.LinkedStack;
+import estructura.pila.Pila;
 public class Codificacion {
 
 	public static void main(String[] args)throws Exception {
@@ -10,10 +15,17 @@ public class Codificacion {
 		Scanner leer = new Scanner(System.in);
 		LinkedList lista= new LinkedList();
 		String palabra=leer.nextLine();
+//		XOR
         //System.out.println(xor(lista, palabra));
-		LinkedList separacion= pasarALista(palabra);
-		System.out.println(separacion);
-		System.out.println(polibio(lista,palabra));
+		
+//		ESte es del polibio
+		//LinkedList separacion= pasarALista(palabra);
+		//System.out.println(separacion);
+		//System.out.println(polibio(lista,palabra));
+		
+//		ESte es del ADFGVX
+		//System.out.println(ADFGVX( lista, palabra));
+
 	}
 	public static void convertirBinario(LinkedList arreglo,String palabra) {
 		for (int i = 0; i < palabra.length(); i++) {
@@ -70,5 +82,59 @@ public class Codificacion {
 		}
         return arreglo;
     }
+	public static LinkedList ADFGVX(LinkedList arreglo,String palabra) throws Exception{
+		 String matriz [][] = {
+					{"-","A", "D", "F", "G", "V", "X"},
+					{"A", "", "", "", "", "", ""},
+				    {"D", "", "", "", "", "", ""},
+				    {"F", "", "", "", "", "", ""},
+				    {"G", "", "", "", "", "", ""},
+				    {"V", "", "", "", "", "", ""},
+				    {"X", "", "", "", "", "", ""}};
+		char[] letrasAl = letrasAleatorias("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray());
+		int k=0;
+		for (int i = 1; i < matriz.length; i++) {
+			for (int j = 1; j < matriz[0].length; j++) {
+				matriz[i][j]=String.valueOf(letrasAl[k]);
+				k++;
+			}
+		} 
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				System.out.print(matriz[i][j]+" ");
+			}
+			System.out.println();
+		} 
+		palabra=palabra.replaceAll(" ", "");
+		System.out.println(palabra);
+		for (int x = 0; x < palabra.length(); x++) {
+			for (int i = 1; i < matriz.length; i++) {
+				 for (int j = 1; j < matriz[0].length; j++) {
+					 if(matriz [i][j].equalsIgnoreCase(String.valueOf(palabra.charAt(x)))){
+						 arreglo.add(String.valueOf(matriz [i][0])+String.valueOf(matriz [0][j]));
+						 i=7;
+						 break;
+					 }
+			      }
+		      }
+		}
+		
+       return arreglo;
+   }
+	public static char[] letrasAleatorias(char[] abecedario) throws Exception{
+		Random random = new Random();
+	    for (int i = abecedario.length - 1; i > 0; i--) {
+	        int index = random.nextInt(i + 1);
+	        if (index != 36) {
+	            char letra = abecedario[index];
+	            abecedario[index] = abecedario[i];
+	            abecedario[i] = letra;
+	        }else {
+	        	i++;
+	        }
+	    }
+	    return abecedario;
+      
+  }
 
 }
